d7f23b9648840d1430543c812f71181bbd4f5bae
Israel <israel@mammon.tiamat>
2014-07-20 14:16:07 +0300

    Non random.

--- snaz/heat.py
+++ snaz/heat.py
@@ -1,32 +1,29 @@
 #!/usr/bin/python3
 
 import numpy as np
-from scipy.interpolate import griddata
 
-coords = [
-    [0, 0],
-    [0, 100],
-    [100, 0],
-    [100, 100],
-    [.5, .1],
-    [1, 1],
-    [40, 31],
-    [1, 1],
-]
-x, y= [x for x, y in coords], [y for x, y in coords]
+x = y = np.arange(0, 100)
 minlng, maxlng = min(x), max(x)
 minlat, maxlat = min(y), max(y)
-extent = [minlng, maxlng, minlat, maxlat]
+x, y = np.meshgrid(x, y)
 
-z = np.random.randn(len(coords)) + 10
+color = np.zeros(x.shape)
 
-xi = np.linspace(minlng, maxlng, 300)
-yi = np.linspace(minlat, maxlat, 300)
-zi = griddata((x, y), z, (xi[None,:], yi[:,None]), method='cubic')
+from matplotlib.mlab import bivariate_normal
+def bomb(lat, lng, width=10, height=10):
+    return bivariate_normal(x, y, width, height, lat, lng)
+
+color += bomb(45, 50, 12, 7)
+color += bomb(55, 60, 15, 5)
+color -= bomb(65, 67, 15, 5)
+color -= bomb(70, 82, 5, 15)
 
 from matplotlib import pyplot as plt, image
 
-plt.contourf(xi, yi, zi, 15, extent=extent, cmap=plt.cm.spectral, alpha=0.5)
+colorful = plt.contourf(x, y, color, 10, alpha=0.3, cmap=plt.cm.spectral)
+contours = plt.contour(colorful, levels=colorful.levels[::2], hold='on')
+
+extent = [minlng, maxlng, minlat, maxlat]
 plt.imshow(image.imread('map.png'), extent=extent)
 
 cb = plt.colorbar()
